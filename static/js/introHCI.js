0ee0846871a83b8a563d857db394ed8dfe3c4a7d
Joey Hoang <j4hoang@ucsd.edu>
2021-01-21 10:49:25 -0800

    added javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -8,10 +8,32 @@ $(document).ready(function() {
 /*
  * Function that is called when the document is ready.
  */
+function projectClick(e) {
+    console.log("Project clicked");
+    e.preventDefault();
+    $(this).css("background-color", "green");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    	if (description.length == 0) {
+       		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    			} else {
+       				$("a.thumbnail").fadeOut();
+				}
+
+}
+
+
+
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("The best way is just to observe the noise of the world.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
+
+
+	$("a.thumbnail").click(projectClick);
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
